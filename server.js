b052e4de03c72ac02892ec16e47ad4cf90718e8d
peterfehertoy <fehertoypeter@gmail.com>
2020-03-28 14:44:42 +0200

    Update

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose'); 
 const app = express();
 const db = require('./config/keys.js').MONGODB_URI
-
 app.use(express.json());
+
+const items = require('./routes/api/items');
+
+
 
 
 // Connect to Mongo
@@ -15,3 +18,6 @@ mongoose.connect(db, {
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!');
 });
+
+// Use Routes
+app.use('/api/items', items);
