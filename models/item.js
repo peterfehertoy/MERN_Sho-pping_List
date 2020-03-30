08198efeecb27988b72627ae47a914970bea8a75
peterfehertoy <fehertoypeter@gmail.com>
2020-03-30 12:27:29 +0300

    React Install
    - React

--- models/item.js
+++ models/item.js
@@ -7,6 +7,10 @@ const ItemSchema = new Schema({
         type:String,
         required: true
     },
+    price: {
+        type:String,
+        required: true
+    },
     date: {
         type: Date,
         default: Date.now
--- routes/api/items.js
+++ routes/api/items.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Item Model
-const item = require('../../models/item');
+const Item = require('../../models/item');
 
 // ITT KÉRJÜK KI A MONGODB BŐL AZ ADATOKAT
 // @route   GET api/items
@@ -10,8 +10,49 @@ const item = require('../../models/item');
 // access   Public
 
 router.get('/', (req, res) => {
-    item.find()
+    Item.find()
     .sort({date: -1 })
-    .then(items => res.json(items))
+    .then(items => res.json(items));
 });
+
+// ITT Csinálunk posztokat a MondoDb-be
+// @route   POST api/items
+// @desc    Create A Post
+// access   Public
+
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+
+// @route PUT api/items/:id
+// @desc Update an Item
+// @access Public
+router.put("/:id", (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.update({ $set:{ 
+        name: req.body.name, 
+        price: req.body.price} })
+    .then(()=> res.json( {success: true})))
+    .catch(err => res.status(404).json({ success: false}));
+    });
+
+
+
+// ITT Törölhetünk posztokat
+// @route   DELETE api/items
+// @desc    Delete A Post
+// access   Public
+
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true}))
+    ).catch(err => res.status(404).json({success: false}));
+});
+
 module.exports = router;
